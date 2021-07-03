e5f1ede40876bdcf0d3191a5098546526beec5b0
Bavani.S <53052023+Bavani-S@users.noreply.github.com>
2021-07-04 00:22:46 +0530

    Committed

--- Spring-REST-Handson(Day 68)/src/main/java/com/example/demo/SpringRestApplication.java
+++ Spring-REST-Handson(Day 68)/src/main/java/com/example/demo/SpringRestApplication.java
@@ -1,16 +1,18 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringRestApplication {
 
-                 private static final Logger LOGGER = LoggerFactory.getLogger(SpringRestApplication.class);
+        private static final Logger LOGGER = LoggerFactory.getLogger(SpringRestApplication.class);
 	public static void main(String[] args) {
-                                   LOGGER.info("Application started...");
+                LOGGER.info("Application started...");
 		SpringApplication.run(SpringRestApplication.class, args);
-                                   LOGGER.info("Process Completed...");
+                LOGGER.info("Process Completed...");
 	}
 
 }
